0710c127746c9c1c53001b93eb57688d5de27880
kevin zhang <myqq@qq.com>
2024-01-31 10:51:04 +0800

    update vm spec

--- multipass/launch-2vm.sh
+++ multipass/launch-2vm.sh
@@ -1,3 +1,3 @@
-multipass launch 20.04 --name master-k8s -c 2 -m 4G -d 25G
-multipass launch 20.04 --name worker-k8s -c 2 -m 4G -d 25G
+multipass launch 20.04 --name master-k8s -c 4 -m 4G -d 35G
+multipass launch 20.04 --name worker-k8s -c 4 -m 4G -d 35G
 
